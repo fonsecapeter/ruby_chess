84df94da4b2da4cc44cd87519e9b9ad3ecb4c09f
fonseapeter <peter.nfonseca@gmail.com>
2016-07-19 18:20:10 -0700

    added visible available moves and unicode players

--- lib/display.rb
+++ lib/display.rb
@@ -1,5 +1,6 @@
 require 'colorize'
 require_relative 'modules/cursorable'
+require 'byebug'
 
 class Display
   include Cursorable
@@ -15,13 +16,19 @@ class Display
 
   def render
     system("clear")
-    # 9.times { print "\x1B[1A\x1B[2K" } <= very noticable
+    # 9.times { print "\x1B[1A\x1B[2K" } <= very noticable alt to clear
     draw_board.each { |row| puts row.join }
   end
 
   private
   def draw_board
-    output = []
+    if @selected
+      @available_spaces = @board[@selected].valid_moves(@board)
+    else
+      @available_spaces = []
+    end
+
+    output = [["           🐧            ".colorize(background: :red, color: :white)]]
     @board.grid.each_with_index do |row, i|
       output_row = []
       row.each_with_index do |space, j|
@@ -29,20 +36,23 @@ class Display
       end
       output << output_row
     end
-    output
+    output << ["           🐘            ".colorize(background: :green, color: :black)]
   end
 
   def colors_for(i, j)
     if [i, j] == @cursor_pos
       bg = @player_color
+    elsif @available_spaces.include?([i, j])
+      bg = :dark_white
     elsif (i + j).even?
       bg = :blue
     else
       bg = :light_black
     end
 
+
     if [i, j] == @selected
-      clr = :blue
+      clr = :light_purple
     else
       clr = @board[[i, j]].color
     end
